917f11e8db43fc64c5d568dd5ed700a9b150eb27
Daria <dariakuptsova15@gmail.com>
2021-05-04 12:42:15 +0300

    some changes

--- get_next_line.c
+++ get_next_line.c
@@ -30,13 +30,51 @@ char	*ft_strdup(const char *s1)
 	return (dst);
 }
 
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	size_t	i;
+	size_t	j;
+	char	*dst;
+
+	if (!s1 || !s2)
+		return (NULL);
+	dst = (char *)malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
+	if (!dst)
+		return (NULL);
+	i = 0;
+	while (s1[i] != '\0')
+	{
+		dst[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (s2[j] != '\0')
+		dst[i++] = s2[j++];
+	dst[i] = '\0';
+	return (dst);
+}
+
+/*char	ft_strchr(const char *str, int ch)
+{
+	while (*str != ch && *str != '\0')
+		str++;
+	if (*str == ch)
+		return ((char *)str);
+	return (0);
+}*/
+
 int get_next_line(int fd, char **line)
 {
     char buf[BUFFER_SIZE + 1];
+	int len;
 
-    read(fd,buf, BUFFER_SIZE);
-    buf[BUFFER_SIZE] = '\0';
-    *line = ft_strdup(buf);
+	while ((len = read(fd, buf, BUFFER_SIZE)))
+	{
+		buf[len] = '\0';
+		*line = ft_strjoin(*line, buf);
+		
+		
+	}
 
     return (0);
 }
