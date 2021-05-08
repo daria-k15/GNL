3769909038ed353ccfaee5e87cdd3c878f28b06b
Daria <dariakuptsova15@gmail.com>
2021-05-08 12:17:26 +0300

    before pull

--- get_next_line.c
+++ get_next_line.c
@@ -40,66 +40,7 @@ char    *ft_strcpy(char *dst, const char *src)
     dst[i] = '\0';
     return (dst);
 }
-size_t	ft_strlen(const char *str)
-{
-	size_t	i;
 
-	i = 0;
-	while (str[i] != '\0')
-		i++;
-	return (i);
-}
-
-char	*ft_strdup(const char *s1)
-{
-	size_t	i;
-	char	*dst;
-
-	i = 0;
-	dst = (char *)malloc(ft_strlen(s1) + 1);
-	if (!dst)
-		return ((void *)0);
-	while (s1[i] != '\0')
-	{
-		dst[i] = s1[i];
-		i++;
-	}
-	dst[i] = '\0';
-	return (dst);
-}
-
-char	*ft_strjoin(char const *s1, char const *s2)
-{
-	size_t	i;
-	size_t	j;
-	char	*dst;
-
-	if (!s1 || !s2)
-		return (NULL);
-	dst = (char *)malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
-	if (!dst)
-		return (NULL);
-	i = 0;
-	while (s1[i] != '\0')
-	{
-		dst[i] = s1[i];
-		i++;
-	}
-	j = 0;
-	while (s2[j] != '\0')
-		dst[i++] = s2[j++];
-	dst[i] = '\0';
-	return (dst);
-}
-
-char	*ft_strchr(const char *str, int ch)
-{
-	while (*str != ch && *str != '\0')
-		str++;
-	if (*str == ch)
-		return ((char *)str);
-	return (0);
-}
 
 
 /*int get_line(int fd, char **line)
@@ -179,12 +120,13 @@ int get_next_line(int fd, char **line)
 }
 
 
-/*int main()
+int main()
 {
     int     fd;
     char    *line;
-    fd = open ("text.txt", O_RDONLY);
+    fd = open ("alphabet", O_RDONLY);
     while (get_next_line(fd, &line))
         printf("%s\n", line);
     return (0);
-}*/
+}
+
